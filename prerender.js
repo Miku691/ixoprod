045017a5b0b8c31a1ff5c624f99f9877d2eff755
Makaradhwaja Behera (Miku) <57888483+Miku691@users.noreply.github.com>
2025-02-27 16:29:43 +0530

    Update prerender.js

--- prerender.js
+++ prerender.js
@@ -16,11 +16,17 @@ const routes = [
   const page = await browser.newPage();
   
   for (const route of routes) {
-    await page.goto(`https://miku691.github.io/ixoquiz${route}`, { waitUntil: 'networkidle0' });
+    await page.goto(`https:www.infoxodia.com${route}`, { waitUntil: 'networkidle0' });
     const content = await page.content();
     const filePath = path.join(__dirname, 'dist', route === '/' ? 'index.html' : `${route}.html`);
     fs.mkdirSync(path.dirname(filePath), { recursive: true });
-    fs.writeFileSync(filePath, content);
+    
+    // Check if the file exists, then write or update the content
+    if (fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, content);
+    } else {
+      fs.writeFileSync(filePath, content);
+    }
   }
   
   await browser.close();
